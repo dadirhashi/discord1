c17eb93008a375f5a6b6ff5149945cac1e8f7c06
Abdulkadir Hashi <dadirhashi544@gmail.com>
2025-09-10 00:02:25 +0200

    g

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World ,nfdfll´hghfgfgfgyjyjn !");
+            Console.WriteLine("Hello, World ,nfdfll´hghfgfgfggyjyjn !");
         }
     }
 }
